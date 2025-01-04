8f8d64c5e4588e585a895a002ae41ebd22083b49
Gabriel Floriano <gabriel.floriano@outlook.com.br>
2025-01-04 20:40:03 -0300

    chore: changing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const { VueLoaderPlugin } = require("vue-loader");
 const Dotenv = require('dotenv-webpack');
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8083/",
+    publicPath: argv.mode === "development"
+      ? "http://localhost:8083/"
+      : "https://federated-host.vercel.app/",
   },
 
   resolve: {
